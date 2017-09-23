4e2814bb15aeb244d8571e8c3c8dfa6763dfe733
Jeff Wu <wuthefwasthat@gmail.com>
2017-09-22 19:19:23 -0700

    wip

--- convert.py
+++ convert.py
@@ -4,6 +4,11 @@ def char2num(char):
     assert len(char) == 1
     return ord(char.lower()) - ord('a') + 1
 
+def char2trits(char):
+    num = char2num(char)
+    trits = [num // 9, (num % 9) // 3, num % 3]
+    return trits
+
 def summod(nums):
     return ((sum(nums) - 1) % 26) + 1
 
@@ -20,7 +25,7 @@ def sum_play(play):
         elif card == 'd':
             return 'd'
         elif card == 'D':
-            nums.append(-1)
+            return 'd'
         elif card == 'P':
             continue
         elif card == 'T':
@@ -53,7 +58,7 @@ def sum_trick(trick):
                 assert len(trick) == 1
                 return None
             elif card == 'D':
-                nums.append(-1)
+                nums.append(char2num('d'))
             elif card == 'P':
                 continue
             elif card == 'T':
@@ -187,12 +192,43 @@ rounds = [
     [ # HUSBAND 
         ['1P345678'],
         ['66699', 'JJJ77'],
-        ['99444', '55533', '33KKK'],
-        ['22', '-', '88', 'QQ', 'AA'],
+        ['88222', '55533', '33KKK'],
+        ['44', '-', '99', 'QQ', 'AA'],
         ['7', '8', 'K', 'A'],
         ['2', 'J', 'A', '-', 'TTTT'],
         ['d'],
     ],
+    [ # GOOD-WILLED
+        ['345678'],
+        ['JJJ44'],
+        ['12345'],
+        ['d'],
+        ['456789T'],
+        ['9P'], 
+        ['66'],
+        ['Q'],
+        [''],
+        ['D'],
+    ],
+    [ # AND WIFE
+        ['1', 'A'],
+        ['6789T'],
+        ['d'],
+        ['-'],
+        ['456789T'],
+        ['9P'], 
+        ['66'],
+        ['Q'],
+    ],
+    # [ # HUSBAND 
+    #     ['????????'],
+    #     ['???99', 'JJJ77'],
+    #     ['99444', '55533', '33???'],
+    #     ['22', '-', '88', 'QQ', 'AA'],
+    #     ['7', '8', '?', 'A'],
+    #     ['2', 'J', 'A', '-', 'TTTT'],
+    #     ['d'],
+    # ],
     # AND WIFE
 ]
 
@@ -204,34 +240,116 @@ for round in rounds:
         sum_play(trick[-1]) for trick in round
     ))
 
-validate_round(rounds[1], True)
+validate_round(rounds[-1], True)
 print(list(
     # sum_trick(trick) for trick in rounds[1]
-    sum_play(trick[-1]) for trick in rounds[1]
+    sum_play(trick[-1]) for trick in rounds[-1]
 ))
 
 print()
 poem = [
-    'teach us, in life',
+    'teach us,          in life',
      # 'to be'
-    'lucky and skilled',        # mahjong, sword
+    'lucky        and   skilled',        # mahjong, sword
      # 'to be'
-    'wise and good willed',     # star, dog
+    'wise         and   good willed',     # star, dog
      # 'to be'
-    'husband and wife',         # dragon, phoenix
+    'husband      and   wife',         # dragon, phoenix
      # 'to have'
-    'grand stakes fulfilled',   # jade, pagoda 
+    'grand vows         fulfilled',   # jade, pagoda 
 ]
 for line in poem:
     print(line)
     print(' '.join(str(char2num(x)) for x in line))
 
+class colors:
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    END = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def char2card(x):
+    if x == ' ' or x == ',':
+        return x
+    num = char2num(x)
+    card = (num - 1) % 13 + 1
+    color = colors.RED if num > 13 else colors.GREEN
+    cardstr = str(card)
+    if card == 1:
+        cardstr = 'A'
+    elif card == 10:
+        cardstr = 'T'
+    elif card == 11:
+        cardstr = 'J'
+    elif card == 12:
+        cardstr = 'Q'
+    elif card == 13:
+        cardstr = 'K'
+    return color + cardstr + colors.END
+
+
+for line in poem:
+    print(line)
+    print(''.join(char2card(x) for x in line))
+
+# MECHANICS (meh):
+#   sum of lead numbers mod 26 (mahjong)
+#   num passes in trinary (dog)
+#   permutation of suits of numbers (phoenix)
+#   winning numbers (dragon)
+
 """
-                 TEACH US          IN LIFE,     100-0
-to be            _____ ___         _______,     200-0
-to be            _____ ___     ____-______,     400-0
-to become        _______          ___ ____,     600-0
-to have          _____ ____      _________.    1000-0
+                 TEACH US          IN LIFE,    
+to be            _____ ___         _______,    
+to be            _____ ___     ____-______,    
+to become        _______          ___ ____,    
+to have          _____ ____      _________.    
 """
 
 print(u"")
+
+"""
+[0, 1, 2, 3] = A
+[0, 1, 3, 2] = B
+[0, 2, 1, 3] = C
+[0, 2, 3, 1] = D
+[0, 3, 1, 2] = E
+[0, 3, 2, 1] = F
+[1, 0, 2, 3] = G
+[1, 0, 3, 2] = H
+[1, 2, 0, 3] = I
+[1, 2, 3, 0] = J
+[1, 3, 0, 2] = K
+[1, 3, 2, 0] = L
+[2, 0, 1, 3] = M
+[2, 0, 3, 1] = N
+[2, 1, 0, 3] = O
+[2, 1, 3, 0] = P
+[2, 3, 0, 1] = Q
+[2, 3, 1, 0] = R
+[3, 0, 1, 2] = S
+[3, 0, 2, 1] = T
+[3, 1, 0, 2] = U
+[3, 1, 2, 0] = V
+[3, 2, 0, 1] = W
+[3, 2, 1, 0] = X
+"""
+
+# for i in 'abcdefghijklmnopqrstuvwxyz':
+#     print(char2trits(i))
+
+"""
+1: 123456789QQKAD
+3: 223356789JJJJA
+2: 2355668899TTdP
+4: 44477TTQQKKKAA
+
+1: 3459JQ
+2: 68QA
+
+1: 3459JQ
+2: 68QA
+"""
